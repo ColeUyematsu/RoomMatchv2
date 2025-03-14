4a732422551e0c83e74c1303948083931d00ae74
ColeUyematsu <cjuyematsu@gmail.com>
2025-03-14 13:38:41 -0700

    deleted files for async matching

--- backend/app/schedule_task.py
+++
@@ -1,9 +0,0 @@
-from celery_worker import run_matching_algorithm
-from datetime import datetime, timedelta
-
-# Schedule the task for 15 minutes from now
-eta_time = datetime.utcnow() + timedelta(minutes=2)
-
-# Queue the task
-result = run_matching_algorithm.apply_async(eta=eta_time)
-
